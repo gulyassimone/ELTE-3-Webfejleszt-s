37981d3081e4303ccfd7570e38c6523c3f2e8b28
Gulyás Simone <gulyassimone@gmail.com>
2020-11-17 19:32:58 +0100

    add enums

--- assignment_1_js_game/main.js
+++ assignment_1_js_game/main.js
@@ -11,102 +11,162 @@ const addCardsCheckbox = document.querySelector("#add_cards_checkbox");
 const play = document.querySelector("#play");
 const tableContainer = document.querySelector('#table-container')
 
+const shapes = {
+        DIAMOND: {magyar:"rombusz" ,english:"diamond" ,code:'D' },
+        OVAL: {magyar:"ovális" ,english:"oval" ,code:'P' },
+        SQUIGGLE: {magyar:"hullámos" ,english:"squiggle" ,code:'S' }
+    };
+
+const colors ={
+    GREEN: {magyar:"zöld" ,english:"green" ,code:'g' },
+    PURPLE: {magyar:"lila" ,english:"purple" ,code:'p' },
+    RED: {magyar:"piros" ,english:"red" ,code:'r' }
+}
+const fills ={
+    SOLID: {magyar:"teli" ,english:"solid" ,code:"S", },
+    STRIPPED: {magyar:"csíkos" ,english:"stripped" ,code:"H" },
+    OPEN: {magyar:"üres" ,english:"open" ,code:"O" }
+}
+
 class players {
     constructor(name, point, status) {
-        this.name = name;
-        this.point = point;
-        this.status = status;
+        this._name = name;
+        this._point = point;
+        this._status = status;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get point() {
+        return this._point;
+    }
+
+    get status() {
+        return this._status;
     }
 
     pointIncreasing() {
-        this.point = point - 1;
+        this._point = point - 1;
     }
 
     pointDecreasing() {
-        this.point = point + 1;
+        this._point = point + 1;
     }
 }
 
 class Card {
-    constructor(form, color, number, fill, id, index) {
-        this.form = form;
-        this.color = color;
-        this.number = number;
-        this.fill = fill;
-        this.id = id;
-        this.index = index;
-        this.selected = 0;
+    constructor(shape, color, number, fill, id) {
+        this._selected = 0;
+        this._shape = shape;
+        this._color = color;
+        this._number = number;
+        this._fill = fill;
+        this._id = id;
+    }
+    get shape() {
+        return this._shape;
+    }
+
+    get color() {
+        return this._color;
+    }
+
+    get number() {
+        return this._number;
+    }
+
+    get fill() {
+        return this._fill;
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    get selected() {
+        return this._selected;
+    }
+    writeToConsole() {
+        console.log(`id = ${this._id} number = ${this._number} form = ${this._shape} color = ${this._color} fill = ${this._fill} selected = ${this._selected}`);
     }
 
     select() {
-        this.selected = 1;
+        this._selected = 1;
     }
 
     back() {
-        this.selected = 0;
+        this._selected = 0;
     }
 
     drop() {
-        this.selected = -1;
+        this._selected = -1;
     }
 }
 
-class Deck {
-    constructor(number) {
-        this.pack = pack;
-    }
+const deck = {
+    deck: [],
 
-    deal() {
-
-    }
-
-    suffle() {
-
-    }
-
-    drop() {
-
-    }
-}
-
-const game = {
-    matrix: [],
-    table: 0,
 
     init: function () {
         const n = 3;
         const m = 4;
 
-        this.matrix = this.generateMatrix(n, m);
-        console.log(this.matrix);
+        this.generateDeck();
 
-        this.createTable(this.matrix);
+        this.createTable();
     },
-    createTable: function (data) {
+    createTable: function () {
+        this.deck.forEach(element => element.writeToConsole());
         let table = document.createElement('table');
-        data.forEach(function (rows) {
+        for(let i=0; i<4; i++){
             const row = document.createElement('tr');
-            rows.forEach(function (cells) {
+            for(let j=0; j<3; j++){
                 const cell = document.createElement('td');
-                cell.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" /></svg>';
+                console.log("deck" + this.deck[i+j])
+                cell.innerHTML = '<div class="deck"><img src=./icons/1HgD.svg alt="some file"/></div>';
                 row.appendChild(cell);
-            });
+            }
             table.appendChild(row);
-        });
-        tableContainer.appendChild(table);
+        }
     },
 
-    generateMatrix: function (n, m) {
-        const matrix = []
-        for (let i = 0; i < n; i++) {
-            const row = []
-            for (let j = 0; j < m; j++) {
-                row.push(0)
+    generateDeck: function () {
+        let deck
+
+        for(let i=1; i<5; ++i){
+            console.log();
+            for(const shape in shapes){
+                for(const color in colors){
+                    for(const fill in fills){
+                        const card = new Card(shape,color,i,fill,`${i}${fills[fill].code}${colors[color].code}${shapes[shape].code}`);
+                        this.deck.push(card);
+                    }
+                }
             }
-            matrix.push(row)
         }
-        return matrix
+    },
+
+//    deal() {
+//    }
+    shuffle: function () {
+        for (let i = this.deck.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * i);
+            let temp = this.deck[i];
+            this.deck[i] = this.deck[j];
+            this.deck[j] = temp;
+        }
     }
+    //   drop() {
+    //   }
+}
+
+const game = {
+
+    init: function () {
+        deck.init();
+    },
 }
 
 showRules.addEventListener("click", function (event) {
@@ -146,7 +206,6 @@ play.addEventListener("click", function (event) {
     gameDiv.style.display = "block";
     game.init();
 })
-
 
 
 /*
